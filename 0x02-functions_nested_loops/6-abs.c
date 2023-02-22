3821d78905d3d92bbe012446b3deaf58f1cb63a1
patjunior <mphorabs5@gmail.com>
2023-02-22 09:53:30 -0800

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,12 @@
 
 int _abs(int c)
 
+/**
+ * _abs - Computes the absolute value of the integer.
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
+ */
+
 { 
 	if (c < 0)
 	{ 
@@ -11,5 +17,4 @@ int _abs(int c)
 	return(abs_val);
 	}
 	return(c);
-
 }
