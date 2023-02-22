db467fc7346c6160214d9dcd7378942f0e04d7c6
patjunior <mphorabs5@gmail.com>
2023-02-22 11:03:16 -0800

    add

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+void print_to_98(int n)
+
+{
+	if (n <= 98)
+	{
+	for (; n<=98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n");
+	}
+	}
+	}
+	else 
+	{
+	for (; n>= 98; --)
+	{
+	if (n == 98)
+
+
+
+}
+}
+}
+
