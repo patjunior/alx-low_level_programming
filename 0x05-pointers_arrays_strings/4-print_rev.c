aa5aa3f550613732df92851c82bfd3a5ae4a2a0b
patjunior <mphorabs5@gmail.com>
2023-03-07 06:49:21 -0800

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,18 @@
 #include "main.c"
 
-int main(void)
-{
-	char *str;
+/**
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
+ */
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return (0);
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
